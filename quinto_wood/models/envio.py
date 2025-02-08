60099e811bb04a6ec316cba7314668f7dae11369
PabloJH97 <pablojhurtadohidalgo@gmail.com>
2025-02-08 17:51:26 +0100

    Update envio.py

--- quinto_wood/models/envio.py
+++ quinto_wood/models/envio.py
@@ -9,7 +9,7 @@ class Envio(models.Model):
     _description = 'Quinto_wood Envio'
 
     direccion = fields.Text(string="Dirección", required=True, help="Dirección de envío")
-    cpostal = fields.Integer(string="Código Postal", size=5 required=True, help="Código postal del envío")
+    cpostal = fields.Integer(string="Código Postal", size=5, required=True, help="Código postal del envío")
     fechaEnvio = fields.Datetime(string="Fecha de envío", required=True, help="Fecha en la que se envió el envío")
     fechaLlegada = fields.Datetime(string="Fecha de llegada", help="Fecha en la que llegará el envío")
     estado = fields.Selection([('preparacion','En preparación'),
